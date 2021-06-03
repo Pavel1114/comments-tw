ba6dbd0a511e7525d2cc03473487a4b00b4893c5
Karpets Pavel <karpets.pavel@gmail.com>
2021-06-03 17:20:39 +0600

    add debug_toolbar

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,1 +1,11 @@
 from .base import *  # noqa
+from .base import INSTALLED_APPS, MIDDLEWARE
+
+
+def show_toolbar(request):
+    return True
+
+
+MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
+INSTALLED_APPS += ["debug_toolbar"]
+DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": "config.settings.development.show_toolbar"}
