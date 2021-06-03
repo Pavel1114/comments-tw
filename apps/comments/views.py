2a8353687623d0786ef73c53493fbd949c6279e8
Karpets Pavel <karpets.pavel@gmail.com>
2021-06-03 21:53:41 +0600

    add filter_queryset to CommentsViewSet

--- apps/comments/views.py
+++ apps/comments/views.py
@@ -7,3 +7,20 @@ from apps.comments.serializers import CommentSerializer
 class CommentsViewSet(ModelViewSet):
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
+
+    def filter_queryset(self, queryset):
+        queryset = super().filter_queryset(queryset)
+        query_params = self.request.query_params
+        if "comment_id" in query_params:
+            try:
+                root = Comment.objects.get(id=int(query_params["comment_id"]))
+                queryset = queryset.filter(tree_id=root.tree_id, level__gt=root.level)
+            except Comment.DoesNotExist:
+                queryset = Comment.objects.none()
+        if "entity_type" in query_params:
+            queryset = queryset.filter(
+                entity_type_id=query_params["entity_type"],
+            )
+            if "entity_id" in query_params:
+                queryset = queryset.filter(entity_id=query_params["entity_id"])
+        return queryset
