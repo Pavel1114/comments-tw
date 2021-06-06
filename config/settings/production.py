305a499ffdf2e2b2d070919a5c58773491d8e7f4
Karpets Pavel <karpets.pavel@gmail.com>
2021-06-06 19:04:30 +0600

    add whitenoise

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,1 +1,6 @@
 from .base import *  # noqa
+from .base import BASE_DIR, MIDDLEWARE
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MIDDLEWARE.insert(2, "whitenoise.middleware.WhiteNoiseMiddleware")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
